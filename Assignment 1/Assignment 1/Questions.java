7fe0eb4e4a266a0848254fc2ed6252f6cc14324e
Savan <Savan777@users.noreply.github.com>
2019-10-04 13:39:19 -0400

    Update Questions.java
    added additional comments.

--- Assignment 1/Assignment 1/Questions.java
+++ Assignment 1/Assignment 1/Questions.java
@@ -3,11 +3,14 @@
  * 
  */
 
+// this class will be used to create and object of type questions
+// when the server reads the questions file, it will store those values into this object.
 public class Questions {
 	
 	private int questionNumber;
 	private String question, answer;
 	
+	//constructor
 	public Questions(int number, String question, String answer) {
 		questionNumber = number;
 		this.question = question;
